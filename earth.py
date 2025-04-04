26afe17e7bd75a58d13860a129f2844bfa6a573b
vbenedit30 <vbenedit@mail.austral.edu.ar>
2025-04-04 12:27:26 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -7,3 +7,4 @@ def earth():
 
     print(f"The result of {country_x} comes first in the dictionary than {country_y} is {resultado_x_first}.")
     print(f"The result of {country_y} comes first in the dictionary than {country_x} is {resultado_y_first}.")
+earth()
